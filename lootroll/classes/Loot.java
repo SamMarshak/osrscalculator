3482209ffb72374be7be16436d332bfcfb13dd84
Sam Marshak <sammarshak@gmail.com>
2024-10-18 12:23:45 -0700

    Not much made in terms of code, most of my time working on it in class was spent on researching :P

---
+++ lootroll/classes/Loot.java
@@ -0,0 +1,10 @@
+public class Loot {
+
+    /*
+     * Loot, Class. Shouldn't have tons of subclasses.
+     * 
+     * -isUnique boolean (T if Unique to boss table. F if standard)
+     * -String name (This might just be the identifier). Might change to a huge library of id, item pairing things
+     * 
+     */
+}
